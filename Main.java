2d8ac505ed5e3a373aada947770e2ac31223f34a
dipesh1234 <dipeshacharya725@gmail.com>
2023-06-15 22:30:33 +0545

    commenting out data types

--- Main.java
+++ Main.java
@@ -3,10 +3,10 @@ import java.time.LocalDateTime;
 public class Main {
     public static void main(String[] args){
         int age = 30;
-        long viewsCount = 1_234_567_890L;
-        float price = 10.23F;
-        char letter = 'A';
-        boolean isEligible = true;
+        // long viewsCount = 1_234_567_890L;
+        // float price = 10.23F;
+        // char letter = 'A';
+        // boolean isEligible = true;
 
         System.out.println(age);
 
