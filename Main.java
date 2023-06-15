ed4a58df5300079eede59685292df14808f847e1
dipesh1234 <dipeshacharya725@gmail.com>
2023-06-15 22:58:11 +0545

    adding another class in java file

--- Main.java
+++ Main.java
@@ -16,3 +16,12 @@ public class Main {
 
     }
 }
+
+class primitiveTypes{
+    public static void main(String[] args){
+       byte x=1;
+       byte y=x;
+       x=2;
+       System.out.println(x);
+    }
+}
