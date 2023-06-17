d46c55d2239b31ef1defd1599b086dcc2ff09656
dipesh1234 <dipeshacharya725@gmail.com>
2023-06-17 23:31:54 +0545

    arithmeticExpression file added

---
+++ arithmiticExpressions.java
@@ -0,0 +1,15 @@
+public class arithmiticExpressions {
+    public static void main(String[] args){
+        double result = (double)10/(double)3;
+        System.out.println(result);
+
+        //increment operator
+        int x= 1;
+        int y = x++;
+        System.out.println(x);
+        System.out.println(y);
+        
+        int a = (4+2)*3/6;
+        System.out.println(a);
+    }
+}
