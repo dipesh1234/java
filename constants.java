6b69adef0f8ba0c9535fec37e9e850ac463f29d9
dipesh1234 <dipeshacharya725@gmail.com>
2023-06-17 20:48:07 +0545

    constants file added

---
+++ constants.java
@@ -0,0 +1,6 @@
+public class constants {
+    public static void main(String[] args){
+        final float PI = 3.14F; //final is used to make the variable PI a constants and F is used to denote 3.14 as floating 
+        System.out.println(PI);
+    }
+}
