9ff0ad8a1c0f4c0cd9781dbc688331556b7ce033
dipesh1234 <dipeshacharya725@gmail.com>
2023-06-17 20:13:59 +0545

    string file added

---
+++ strings.java
@@ -0,0 +1,12 @@
+public class strings {
+    public static void main(String[] args){
+        String message = "  Hello World" + "!!";
+        System.out.println(message);
+        System.out.println(message.toUpperCase());
+        System.out.println(message.toLowerCase());
+        System.out.println(message.concat("new"));
+        System.out.println(message.length());
+        System.out.println(message.trim());
+        
+    }
+}
