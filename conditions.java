1b12a553ead622a4870421012d5860ef5cfc15c2
dipesh1234 <dipeshacharya725@gmail.com>
2023-08-18 16:21:12 +0545

    solving quadratic equation

--- conditions.java
+++ conditions.java
@@ -1,15 +1,35 @@
 import java.util.Scanner;
+import java.math.*;
 
 public class conditions {
     public static void main(String[] args){
         //User enters a number
         Scanner scan = new Scanner(System.in);
-        System.out.print("Input number: ");
-        int input = scan.nextInt();
 
-        //system checks if number is positive or negative
-        String num = (input >= 0)?"Number is poitive" : "Number is negative";
-        System.out.println(num);
+        System.out.print("Input a: ");
+        double a = scan.nextDouble();
 
+        System.out.print("Input b: ");
+        double b = scan.nextDouble();
+
+        System.out.print("Input c: ");
+        double c = scan.nextDouble();
+
+        //solving quadratic equation
+        double result = b*b - 4*a*c;
+
+        if(result > 0){
+            double r1 = (-b + Math.pow(result, 0.5)) / (2*a);
+            double r2 = (-b - Math.pow(result, 0.5)) / (2*a);
+            System.out.println("The roots are " + r1 +" "+ "and"+ " " +r2);
+        }
+        else if(result == 0){
+            double r1 = (-b) / (2*a);
+            System.out.println("the root is " + " " + r1); 
+        }
+        else{
+            System.out.println("the equations has no real roots");
+        }
     }
+    //scanner.close();
 }
