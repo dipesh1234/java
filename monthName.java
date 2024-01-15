b0af3bf9e6ecdc2e7658f8a778e92c0f4fc5e8ec
dipesh1234 <dipeshacharya725@gmail.com>
2024-01-15 16:19:49 +0545

    program to determine name of month based on the number input

---
+++ monthName.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+
+public class monthName {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the month number: ");
+
+        int monthNumber = scan.nextInt();
+
+        switch (monthNumber) {
+            case 1:
+                System.out.print("The month is January.");
+                break;
+
+            case 2:
+                System.out.println("The month is February.");
+                break;
+
+            case 3:
+                System.out.println("The month is March");
+                break;
+
+            case 4:
+                System.out.println("The month is April.");
+                break;
+            
+            case 5:
+                System.out.println("The month is May.");
+                break;
+                
+            case 6:
+                System.out.println("The month is Juen.");
+                break;
+
+            case 7:
+                System.out.println("The month is July.");
+                break;
+
+            case 8:
+                System.out.println("The month is August.");
+                break;
+            
+            case 9:
+                System.out.println("The month is September.");
+                break;
+
+            case 10:
+                System.out.println("The month is October.");
+                break;
+        
+            case 11:
+                System.out.println("The month is November.");
+                break;
+
+            case 12:
+                System.out.println("The month is December.");
+                break;
+
+            default:
+                System.out.println("Enter valid number between 1 and 12");
+                break;
+        }
+        scan.close();
+
+    }
+}
